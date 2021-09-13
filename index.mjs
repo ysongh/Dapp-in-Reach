ea82f963caa27152abbf404cf080edc2f122a3e5
You Song Hou <yousonghou@gmail.com>
2021-09-13 01:59:17 -0400

    add a simple game logic using random()

--- index.mjs
+++ index.mjs
@@ -1,29 +1,33 @@
-import {loadStdlib} from '@reach-sh/stdlib';
+import { loadStdlib } from '@reach-sh/stdlib';
 import * as backend from './build/index.main.mjs';
 const stdlib = loadStdlib(process.env);
 
 (async () => {
-  const startingBalance = stdlib.parseCurrency(100);
+  const startingBalance = stdlib.parseCurrency(10);
+  const accAlice = await stdlib.newTestAccount(startingBalance);
+  const accBob = await stdlib.newTestAccount(startingBalance);
 
-  const [ accAlice, accBob ] =
-    await stdlib.newTestAccounts(2, startingBalance);
-  console.log('Hello, Alice and Bob!');
-
-  console.log('Launching...');
   const ctcAlice = accAlice.deploy(backend);
   const ctcBob = accBob.attach(backend, ctcAlice.getInfo());
 
-  console.log('Starting backends...');
+  const OUTCOME = ['Bob wins', 'Draw', 'Alice wins'];
+  const Player = (Who) => ({
+    getNum: () => {
+      const num = Math.floor(Math.random() * 100);
+      console.log(`${Who} got ${num}`);
+      return num;
+    },
+    seeOutcome: (outcome) => {
+      console.log(`${Who} got ${OUTCOME[outcome]}`);
+    },
+  });
+
   await Promise.all([
     backend.Alice(ctcAlice, {
-      ...stdlib.hasRandom,
-      // implement Alice's interact object here
+      ...Player('Alice'),
     }),
     backend.Bob(ctcBob, {
-      ...stdlib.hasRandom,
-      // implement Bob's interact object here
+      ...Player('Bob'),
     }),
   ]);
-
-  console.log('Goodbye, Alice and Bob!');
 })();
--- index.rsh
+++ index.rsh
@@ -1,12 +1,41 @@
 'reach 0.1';
 
+const Player = {
+  getNum: Fun([], UInt),
+  seeOutcome: Fun([UInt], Null),
+};
+
 export const main = Reach.App(() => {
-  const A = Participant('Alice', {
-    // Specify Alice's interact interface here
+  const Alice = Participant('Alice', {
+    ...Player,
   });
-  const B = Participant('Bob', {
-    // Specify Bob's interact interface here
+  const Bob   = Participant('Bob', {
+    ...Player,
   });
   deploy();
-  // write your program here
+
+  Alice.only(() => {
+    const handAlice = declassify(interact.getNum());
+  });
+  Alice.publish(handAlice);
+  commit();
+
+  Bob.only(() => {
+    const handBob = declassify(interact.getNum());
+  });
+  Bob.publish(handBob);
+
+  commit();
+
+  each([Alice, Bob], () => {
+    if(handAlice > handBob){
+      interact.seeOutcome(2);
+    }
+    else if(handAlice < handBob){
+      interact.seeOutcome(0);
+    }
+    else{
+      interact.seeOutcome(1);
+    }
+  });
 });
